63725da214a0fad276e1583d5fb4bc8707eb275f
Carlos Cruz Jr. <ccruzjr2005@gmail.com>
2025-02-09 19:30:49 -0500

    adding custom birthday message

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
-console.log("Carlos");
+const myName = 'Carlos';
+let myDay = '2';
+myDay = 'Sunday';
+const message = `Howdy! I'm ${myName} and my special day this year is ${myDay}!`;
+
+console.log(message);
